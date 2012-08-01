e24391736348e33dbb381df95cf52cd465042692
JB Hewitt <jbhewitt@me.com>
2012-08-01 10:34:48 +1000

    modifed import_wam_db to only import from last date

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -26,6 +26,12 @@ class Stat < ActiveRecord::Base
   def import_wam_db
     require 'sqlite3'
 
+    #find last history recorded for machine
+    last_history = History.find_last_by_machine_id(self.machine.id, :order => 'time ASC')
+   # last_import_date_str = last_history.time("%Y/%m/%d")
+    today = Time.now
+    #today_date_str = today.time("%Y/%m/%d")
+
     #decompress 7zip file
     system "cd #{Rails.root}/tmp; 7z -y x #{Rails.root}/public#{self.fileupload}"
  
@@ -34,23 +40,35 @@ class Stat < ActiveRecord::Base
     # snapshot rows = 
     # 0 = "id", 1 = "time", 2 = "window", 3 = "process", 4 = "user"]
     columns = nil
-    db.execute2( "select * from snapshot" ) do |row|
+    puts "select * from snapshot WHERE time BETWEEN '#{last_history.time.to_s}' AND '#{today.to_s}';"
+    db.execute2( "select * from snapshot WHERE time BETWEEN '#{last_history.time.to_s}' AND '#{today.to_s}';" ) do |row|
      if columns.nil?
         columns = row
-      else
+      else        
+        puts row[1]
         time = Chronic.parse(row[1])
-        #time = row[1]
+        #time = DateTime.parse(row[1])
+        puts time
+
         history = History.find_or_create_by_time_and_machine_id(time,self.machine.id)
-        history.window = row[2]
-        history.process = row[3]
-        history.save
+        if history.process.nil?
+          history.window = row[2]
+          history.process = row[3]
+          history.save
+          puts 'SAVING HISTORY FOR #{history.process}'
+        end
         #import application name
         app = Application.find_or_create_by_process(history.process)
         if (app.name.nil?)
           app.name = history.process.split('\\').last
+          app.save
         end
-        app.save
+
       end
     end
+
+    #endtime = Time.new
+
+
   end
 end
